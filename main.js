b7117a98a19d7a15aca0e8efda744a0d828acb91
nickbevers <nick.bevers9@gmail.com>
2022-10-06 22:33:03 +0200

    feat: add donut model to the project; feat: add variables to the main threejs file and setup the camera position

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
 import "./style.css";
 import * as THREE from "three";
 
-let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// Defining variables
+const sizes = { width: window.innerWidth, height: window.innerHeight }
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 
-let renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-let geometry = new THREE.BoxGeometry();
-let material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
-let cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
+// Setting up the camera & renderer
+camera.position.set( 0, 0, 5 );
 
 const animate = () => {
     requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
     renderer.render( scene, camera );
 }
 
