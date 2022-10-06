0596ae79d9210a66774c18a997682fc6ec6ed851
nickbevers <nick.bevers9@gmail.com>
2022-10-06 22:49:54 +0200

    test: test import of dracoloader for compressed models

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import "./style.css";
 import * as THREE from "three";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
 
 // Defining variables
