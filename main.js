1b1264fdff541f98a5898ecb39c3aea9d37f1ca2
nickbevers <nick.bevers9@gmail.com>
2022-10-07 21:08:44 +0200

    fix: changes load function for the donut a callable function to create a donut universe with parameters to change the position, scale and color; fix: change constant texture to logoTextures for better understandability; feat: add for loop to create 10 more donuts with random colors an random positions

--- main.js
+++ main.js
@@ -17,34 +17,7 @@ dracoLoader.setDecoderConfig({ type: 'js' });
 dracoLoader.setDecoderPath( '/node_modules/three/examples/js/libs/draco/' );
 loader.setDRACOLoader(dracoLoader);
 
-
-//Loading the model and adding it to the scene
-loader.load(
-    "donut_compressed.glb",
-
-    ( gltf ) => {
-        const root = gltf.scene;
-        root.traverse( function ( child ) {
-            if ( child.isMesh ) {child.material.color.set( 0xffffff * Math.random() );}
-        });
-
-        root.scale.set(1, 1, 1);
-        scene.add( root );
-    },
-
-    ( xhr ) => {
-        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-    },
-
-    ( error ) => {
-        console.log( 'An error happened' );
-        console.error( error );
-    }
-);
-
-
-// Setting up the white cube for the logo of a customer
-const textures = [
+const logoTexture = [
     new THREE.MeshBasicMaterial({ color: 0xffffff }),
     new THREE.MeshBasicMaterial({ color: 0xffffff }),
     new THREE.MeshBasicMaterial({ color: 0xffffff }),
@@ -54,12 +27,42 @@ const textures = [
 ];
 
 
-const cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
-const cube = new THREE.Mesh( cubeGeometry, textures );
-cube.position.set(0, 0.068, -0.045);
-cube.rotation.set(2.007, 3.14, 3.14);
-cube.scale.set(0.05, 0.03, 0.001);
-scene.add( cube );
+//Loading the model and adding it to the scene
+const loadDonut = (position = [0, 0, 0], scale = [1, 1, 1], colors = false) => {
+    // Load the donut model
+    loader.load(
+        "donut_compressed.glb",
+
+        ( gltf ) => {
+            const root = gltf.scene;
+            if (colors) {
+                root.traverse( ( child ) => {
+                    if ( child.isMesh ) { child.material.color.set(0xffffff * Math.random()) }
+                });
+            }
+            root.scale.set(...scale);
+            root.position.set(...position);
+            scene.add( root );
+        },
+
+        ( xhr ) => {
+            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+        },
+
+        ( error ) => {
+            console.log( 'An error happened' );
+            console.error( error );
+        }
+    );
+
+    // Load the donut logo
+    const cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const cube = new THREE.Mesh( cubeGeometry, logoTexture );
+    cube.position.set(0, 0.068, -0.045);
+    cube.rotation.set(2.007, 3.14, 3.14);
+    cube.scale.set(0.05, 0.03, 0.001);
+    scene.add( cube );
+}
 
 
 // Setting up the lights
@@ -90,4 +93,7 @@ const animate = () => {
     renderer.render( scene, camera );
 }
 
+// Calling the functions
+loadDonut();
 animate();
+for(let i = 0; i < 10; i++) { loadDonut([Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1], [Math.random() * 0.5 + 0.5, Math.random() * 0.5 + 0.5, Math.random() * 0.5 + 0.5], true) };
